c24feef0263ffbe037d11cb69767f3b0a16a2bf7
Jay <jay@Jays-MacBook-Pro.local>
2020-03-05 15:28:16 -0800

    First Website commit

---
+++ my_website.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for
+import os
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return render_template(
+        "home_page.html"
+    )
+
+@app.route('/test')
+def testing():
+    return 'Test Page'
+
+@app.route('/test/<name>')
+def name_html(name):
+    html = '<h1>This is ' + name.title() + "'s Website!</h1>"
+    html += '\n' + '<h2>:P</h2>'
+    return html
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
