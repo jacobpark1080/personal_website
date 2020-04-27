101732b3bd1eeab4c9b1a367b474d561b19685f0
Jacob Park <jay@Jacobs-MacBook-Pro.local>
2020-04-26 18:35:34 -0700

    merging master with err and adding an api project

--- my_website.py
+++ my_website.py
@@ -5,12 +5,39 @@ import json
 
 app = Flask(__name__)
 
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
+
 
 @app.route('/')
 def home():
     return render_template(
         "home_page.html"
     )
+
+@app.route('/api', methods=['GET'])
+def apihome():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    return jsonify(books)
 
 @app.route('/photos')
 def photos():
