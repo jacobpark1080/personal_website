38f11a7a295ee29def2c1af5aee3b92342882e91
Jacob Park <jay@Jacobs-MacBook-Pro.local>
2020-04-26 20:08:55 -0700

    Made a simple API connected to a database

--- db/helper.py
+++ db/helper.py
@@ -1,13 +1,14 @@
 import sqlite3
 
-DB_PATH = './db/todo.db'   # Update this path accordingly
+# Database Tutorial
+MY_DB_PATH = './db/todo.db'   # Update this path accordingly
 NOTSTARTED = 'Not Started'
 INPROGRESS = 'In Progress'
 COMPLETED = 'Completed'
 
 def add_to_list(item,status):
     try:
-        conn = sqlite3.connect(DB_PATH)
+        conn = sqlite3.connect(MY_DB_PATH)
         c = conn.cursor()
         c.execute(f"insert into items(item, status, date) values('{item}','{status}',strftime('%m-%d-%Y',date('now')))")
         conn.commit()
@@ -18,7 +19,7 @@ def add_to_list(item,status):
 
 def get_all_items():
     try:
-        conn = sqlite3.connect(DB_PATH)
+        conn = sqlite3.connect(MY_DB_PATH)
         c = conn.cursor()
         c.execute('select * from items')
         rows = c.fetchall()
@@ -29,7 +30,7 @@ def get_all_items():
 
 def get_item(item):
     try:
-        conn = sqlite3.connect(DB_PATH)
+        conn = sqlite3.connect(MY_DB_PATH)
         c = conn.cursor()
         c.execute("select status from items where item='%s'" % item)
         status = c.fetchone()[0]
@@ -40,7 +41,7 @@ def get_item(item):
 
 def update_status(item, status):
     try:
-        conn = sqlite3.connect(DB_PATH)
+        conn = sqlite3.connect(MY_DB_PATH)
         c = conn.cursor()
         if c.execute(f"SELECT EXISTS (SELECT * FROM items WHERE item='{item}')"):
             c.execute(f"update items set status='{status}',date=strftime('%m-%d-%Y',date('now')) where item='{item}'")
@@ -55,7 +56,7 @@ def update_status(item, status):
 
 def delete_item(item):
     try:
-        conn = sqlite3.connect(DB_PATH)
+        conn = sqlite3.connect(MY_DB_PATH)
         c = conn.cursor()
         if c.execute(f"SELECT EXISTS (SELECT * FROM items WHERE item='{item}')"):
             c.execute(f"DELETE FROM items WHERE item='{item}'")
@@ -67,3 +68,63 @@ def delete_item(item):
     except Exception as e:
         print('Error: ', e)
         return None
+
+
+#API Tutorial
+BOOK_PATH = './db/books.db'
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+def api_all():
+    conn = sqlite3.connect(BOOK_PATH)
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_books = cur.execute('SELECT * FROM books;').fetchall()
+    return all_books
+
+
+def api_filter(id=None,published=None,author=None):
+    query = "SELECT * FROM books WHERE"
+    to_filter = []
+
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    if published:
+        query += ' published=? AND'
+        to_filter.append(published)
+    if author:
+        query += ' author=? AND'
+        to_filter.append(author)
+
+    query = query[:-4] + ';'
+
+    conn = sqlite3.connect(BOOK_PATH)
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+
+    results = cur.execute(query, to_filter).fetchall()
+
+    return results
+
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
--- my_website.py
+++ my_website.py
@@ -1,27 +1,10 @@
-from flask import Flask, render_template, url_for, request, Response, redirect
+from flask import Flask, render_template, url_for, request, Response, redirect, jsonify
 import os
 from db import helper
+from db.helper import books
 import json
 
 app = Flask(__name__)
-
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
 
 
 @app.route('/')
@@ -37,7 +20,49 @@ def apihome():
 # A route to return all of the available entries in our catalog.
 @app.route('/api/v1/resources/books/all', methods=['GET'])
 def api_all():
-    return jsonify(books)
+    results = helper.api_all()
+    return jsonify(results)
+
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_filter():
+    query_parameters = request.args
+
+    id = query_parameters.get('id')
+    published = query_parameters.get('published')
+    author = query_parameters.get('author')
+
+    if not (id or published or author):
+        return page_not_found(404)
+        
+    results = helper.api_filter(id,published,author)
+
+    return jsonify(results)
+
+
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    # Create an empty list for our results
+    results = []
+
+    # Loop through the data and match results that fit the requested ID.
+    # IDs are unique, but other fields might return many results
+    for book in helper.books:
+        if book['id'] == id:
+            results.append(book)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
+
+
 
 @app.route('/photos')
 def photos():
@@ -112,6 +137,10 @@ def get_item():
     response = Response(json.dumps(res_data), status=200, mimetype='application/json')
     return response
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
 @app.route('/test')
 def testing():
     return 'Test Page'
@@ -121,6 +150,7 @@ def name_html(name):
     html = '<h1>This is ' + name.title() + "'s Website!</h1>"
     html += '\n' + '<h2>:P</h2>'
     return html
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
